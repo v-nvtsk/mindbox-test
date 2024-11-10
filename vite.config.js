11461c2320ec7d71372d96b25d549aa9c3b548bf
v-nvtsk <v@nvtsk.ru>
2024-11-11 00:24:41 +0300

    fix: deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: './',
   build: {
     emptyOutDir: true,
     rollupOptions: {
